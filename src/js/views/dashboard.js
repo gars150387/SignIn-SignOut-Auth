8ae6beb4c440598562039057dd0f2f26f856b209
Gustavo Rodriguez <gar.santeliz@gmail.com>
2021-07-29 11:28:34 -0400

    setting signing out function

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import { signOut } from "../component/signOut";
 import { useHistory } from "react-router-dom";
+import { signOut } from "../utilities/signOut";
 
 export const Dashboard = () => {
 	const history = useHistory();
 	return (
 		<>
 			<h1>LogedIn</h1>
-			<button>LogOut</button>
+			<button onClick={()=> signOut()}>LogOut</button>
 		</>
 	);
 };
